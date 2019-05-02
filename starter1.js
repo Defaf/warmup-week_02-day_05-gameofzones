046a936c51d3232b5c8f6eecac366a7ba733569a
Defaf <defaf_kamfar@hotmail.com>
2019-05-02 11:23:54 +0300

    Finish the warmup game zone

---
+++ starter1.js
@@ -0,0 +1,22 @@
+
+let counter = 0 ; 
+
+$('.zone').on("mouseenter", function (event){
+    $(event.target).css("background-color","green");
+});
+
+$('.zone').on("mouseout", function (event){
+    $(event.target).css("background-color","white");
+});
+
+
+$('.zone').click(function (event){
+    $(event.target).off();
+    $(event.target).css("background-color","green");
+    $('this').html(counter ++);
+    if(counter == 4 ){
+        alert("You Win!!");
+    }
+    
+});
+
